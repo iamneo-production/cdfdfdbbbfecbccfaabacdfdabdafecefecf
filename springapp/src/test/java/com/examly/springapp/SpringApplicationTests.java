ed722dc77d9f712331337757a36067849dd9d77a
Iamneo <techadmin@iamneo.ai>
2023-08-06 16:00:09 +0000

    Backup Commit

--- springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
+++ springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -14,11 +14,21 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.net.URL;
+import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+
 @SpringBootTest(classes = SpringappApplication.class)
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 public class SpringApplicationTests {
-	Web
+	WebDriver driver=null;
+	ChromeOptions options = new ChromeOptions();
+	driver = new R
 	@Test
     public void test_case1() throws Exception {
 		
